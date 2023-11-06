8f8ae3a371fb184a5c47e9cd661d430fcb51fbbc
SongShaopu1998 <shaopu@berkeley.edu>
2023-11-05 23:42:03 -0800

    test correctness of command

--- skystore_cli.py
+++ skystore_cli.py
@@ -120,7 +120,7 @@ def register(
     if local_test:
         server_addr = "localhost"
     else:
-        server_addr = "15.160.154.191"
+        server_addr = "52.53.200.167"
 
     try:
         with open(register_config, "r") as f:
