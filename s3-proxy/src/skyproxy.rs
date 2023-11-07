a3c6f1e03fac8527dbfdd82e245d44c1ad141e95
SongShaopu1998 <shaopu@berkeley.edu>
2023-11-06 19:38:14 -0800

    change init regions

--- s3-proxy/src/skyproxy.rs
+++ s3-proxy/src/skyproxy.rs
@@ -145,7 +145,7 @@ impl SkyProxy {
             base_path: if local_server {
                 "http://127.0.0.1:3000".to_string()
             } else {
-                "http://54.176.175.11:3000".to_string()
+                "http://13.57.12.1:3000".to_string()
             },
             ..Default::default()
         };
--- skystore_cli.py
+++ skystore_cli.py
@@ -120,7 +120,7 @@ def register(
     if local_test:
         server_addr = "localhost"
     else:
-        server_addr = "54.176.175.11"
+        server_addr = "13.57.12.1"
 
     try:
         with open(register_config, "r") as f:
