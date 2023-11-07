013c70777d74f9be8c0c15d58630a771acbefa08
SongShaopu1998 <shaopu@berkeley.edu>
2023-11-06 21:09:58 -0800

    change ip

--- store-server/experiment/client.py
+++ store-server/experiment/client.py
@@ -207,7 +207,7 @@ def create_instance(
                 export AWS_ACCESS_KEY_ID={aws_credentials()[0]}; \
                 export AWS_SECRET_ACCESS_KEY={aws_credentials()[1]}; \
                 /home/ubuntu/.cargo/bin/cargo build; \
-                nohup /home/ubuntu/.local/bin/skystore init --config {config_file_path} > /dev/null 2>&1 &'
+                nohup /home/ubuntu/.local/bin/skystore init --config {config_file_path} > output.txt > /dev/null 2>&1 &'
         stdout, stderr = server.run_command(cmd1)
         print("stdout:", stdout)
         print("stderr:", stderr)
